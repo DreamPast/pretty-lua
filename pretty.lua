40bb75fd4722b693ee630874eb1ba23bc36eeaf2
Jin Cai <cjon72@outlook.com>
2024-11-01 11:15:27 +0800

    [feat] add pretty options

--- pretty.lua
+++ pretty.lua
@@ -14,20 +14,11 @@ do
     end
 end
 
----Convert a number to a string.
----@type fun(value:number):string
-local function num2string(value)
-    if value ~= value then
-        return "NaN"
-    elseif value == math.huge then
-        return "Inf"
-    elseif value == -math.huge then
-        return "-Inf"
-    elseif isInteger(value) then
-        return ("%d"):format(value)
-    else
-        return ("%.14g"):format(value)
-    end
+---@param value number
+local function checkUsingIntegerFormatter(value)
+    if value == 0 then return string.sub(tostring(value), 1, 2) ~= "-0" end
+    if value ~= value and value * 0.5 == value then return false end
+    return isInteger(value)
 end
 
 ---The largest integer that can be represented without loss of precision.
@@ -65,6 +56,25 @@ local maxSafeInteger = (function()
     if testFloat(val) then return val end -- bfloat16
 end)()
 
+---@generic T
+---@type fun(value:T):T
+local function copyTable(value)
+    local ret = {}
+    for k, v in pairs(value) do ret[k] = v end
+    return ret
+end
+
+
+---Convert a number to a string.
+---@type fun(value:number):string
+local function num2string(value)
+    if checkUsingIntegerFormatter(value) then
+        return ("%d"):format(value)
+    else
+        return ("%.14g"):format(value)
+    end
+end
+
 ---@type table<integer, string>
 local stringQuoteTable = {}
 do
@@ -90,7 +100,6 @@ do
         end
     end
 end
-
 ---Escape string, replacing special characters in the string with escape sequences.
 ---@param str string The string to be escaped
 ---@param quote string|nil The quote string; no quotes will be added if `quote` == `nil`
@@ -122,31 +131,55 @@ local function escapeString(str, quote)
     return table.concat(retTable)
 end
 
-local lineBreakLimit = 64 ---@type integer
-local indentString = "  " ---@type string
-local indentWidth = 2 ---@type integer
-local maximumNilNumberAllowed = 0 ---@type integer
+---@param x any
+---@return string
+local function addressFormatter(x) return "<" .. tostring(x) .. ">" end
+
+---@class _InnerPrettyOption
+---@field nil fun(x:nil):string
+---@field number fun(x:number):string
+---@field string fun(x:string):string
+---@field boolean fun(x:boolean):string
+---@field function fun(x:function):string
+---@field userdata fun(x:userdata):string
+---@field thread fun(x:thread):string
+---@field table fun(x:table):string
+---@field cycleTableFormatter fun(x:table):string
+---@field indentString string
+---@field lineBreakLimit integer
+---@field maximumNilNumberAllowed number
+---@field numberFormatter nil|fun(x:number):nil
+---@field integerFormatter nil|fun(x:integer):nil
+
+local backupGlobalPrettyOption = {
+    ["nil"] = function() return "nil" end,
+    ["number"] = num2string,
+    ["string"] = function(x) return "\"" .. escapeString(x) .. "\"" end,
+    ["boolean"] = function(x) return (x and "true") or "false" end,
+    ["function"] = addressFormatter,
+    ["userdata"] = addressFormatter,
+    ["thread"] = addressFormatter,
+    ["table"] = addressFormatter,
+    cycleTableFormatter = function(x) return "<cycle " .. tostring(x) .. ">" end,
+    indentString = "  ",
+    lineBreakLimit = 64,
+    maximumNilNumberAllowed = 0,
+    numberFormatter = nil,
+    integerFormatter = nil,
+}
+
+---@type _InnerPrettyOption
+local globalPrettyOption = copyTable(backupGlobalPrettyOption)
 
 ---@param value any
+---@param option _InnerPrettyOption
 ---@return string
-local function writeDirect(value)
-    local typ = type(value)
-    if typ == "nil" then
-        return "nil"
-    elseif typ == "number" then
-        return num2string(value)
-    elseif typ == "string" then
-        return "\"" .. escapeString(value) .. "\""
-    elseif typ == "boolean" then
-        return (value and "true") or "false"
-    else
-        return "<" .. tostring(value) .. ">"
-    end
-end
+local function writeDirect(value, option) return option[type(value)](value) end
 
 ---@param tab any[]
+---@param maximumNilNumberAllowed integer
 ---@return integer
-local function getArraySize(tab)
+local function getArraySize(tab, maximumNilNumberAllowed)
     local integerLimit = 1
     local nilNumber = 0
     while integerLimit < maxSafeInteger do
@@ -177,8 +210,9 @@ local typeOrder = {
 ---@param item string[]
 ---@param leadingSpaceLength integer
 ---@param trimSpaceLength integer
+---@param lineBreakLimit integer
 ---@return string|nil
-local function maybeShortTable(item, leadingSpaceLength, trimSpaceLength)
+local function maybeShortTable(item, leadingSpaceLength, trimSpaceLength, lineBreakLimit)
     local arr = {} ---@type string[]
     local size = 4 + leadingSpaceLength ---@type integer
     for _, str in ipairs(item) do
@@ -187,18 +221,18 @@ local function maybeShortTable(item, leadingSpaceLength, trimSpaceLength)
         table.insert(arr, s)
         if size >= lineBreakLimit then return nil end
     end
-    if arr[1] == nil then
-        return "{ }"
-    end
+    if arr[1] == nil then return "{ }" end
     return "{ " .. table.concat(arr, " ") .. " }"
 end
 
 ---@param value any
+---@param option _InnerPrettyOption
 ---@return string|string[]
-local function writeInternal(value)
-    if type(value) ~= "table" then
-        return writeDirect(value)
-    end
+local function writeInternal(value, option)
+    if type(value) ~= "table" then return writeDirect(value, option) end
+    local lineBreakLimit = option.lineBreakLimit
+    local indentString = option.indentString
+    local maximumNilNumberAllowed = option.maximumNilNumberAllowed
 
     local tableVisit = {} ---@type table<table, boolean>
     local integerSet = {} ---@type table<integer, boolean>
@@ -207,18 +241,14 @@ local function writeInternal(value)
     ---@return string|string[]
     local function subWrite(tab, leadingSpace)
         local typ = type(tab)
-        if typ ~= "table" then
-            return writeDirect(tab)
-        end
-        if tableVisit[tab] then
-            return "<cycle " .. tostring(tab) .. ">"
-        end
+        if typ ~= "table" then return writeDirect(tab, option) end
+        if tableVisit[tab] then return option.cycleTableFormatter(tab) end
         tableVisit[tab] = true
         if #leadingSpace >= lineBreakLimit then
             error("leading space is too long")
         end
 
-        local integerSize = getArraySize(tab)
+        local integerSize = getArraySize(tab, maximumNilNumberAllowed)
         local newLeadingSpace = leadingSpace .. indentString
 
         local ret = {} ---@type string[]
@@ -250,7 +280,7 @@ local function writeInternal(value)
                 return
             end
             do
-                local s = maybeShortTable(item, #leadingSpace + #preffix, #newLeadingSpace)
+                local s = maybeShortTable(item, #leadingSpace + #preffix, #newLeadingSpace, lineBreakLimit)
                 if s ~= nil then
                     pushString(preffix .. s)
                     return
@@ -286,7 +316,7 @@ local function writeInternal(value)
         end)
 
         for _, key in pairs(keyArray) do
-            push(subWrite(tab[key], newLeadingSpace), string.format("[%s] = ", writeDirect(key)))
+            push(subWrite(tab[key], newLeadingSpace), string.format("[%s] = ", writeDirect(key, option)))
         end
 
         if line[2] ~= nil then
@@ -301,34 +331,134 @@ local function writeInternal(value)
 
     local ret = subWrite(value, indentString)
     if type(ret) == "string" then return ret end
-    local s = maybeShortTable(ret, 0, indentWidth)
+    local s = maybeShortTable(ret, 0, #indentString, lineBreakLimit)
     return s or string.format("{\n%s\n}", table.concat(ret, "\n"))
 end
 
-local function printOne(value)
-    local ret = writeInternal(value)
+---@param value any
+---@param option _InnerPrettyOption
+local function printOne(value, option)
+    local ret = writeInternal(value, option)
     if type(ret) == "string" then
         io.write(ret)
         return
     end
-    local s = maybeShortTable(ret, 0, indentWidth)
+    local s = maybeShortTable(ret, 0, #option.indentString, option.lineBreakLimit)
     if s ~= nil then io.write(s) end
     io.write("{\n")
     for _, v in ipairs(ret) do
         io.write(v)
-        io.write('\n')
+        io.write("\n")
     end
     io.write("\n}")
 end
 
+---@class PrettyOption
+---@field integerFormatter nil|fun(x:integer):string
+---@field numberFormatter nil|fun(x:number):string
+---@field stringFormatter nil|fun(x:string):string
+---@field booleanFormatter nil|fun(x:boolean):string
+---@field functionFormatter nil|fun(x:function):string
+---@field userdataFormatter nil|fun(x:userdata):string
+---@field threadFormatter nil|fun(x:thread):string
+---@field indentWidth nil|integer
+---@field lineBreakLimit nil|integer
+---@field maximumNilNumberAllowed nil|number
+
+---@generic T
+---@param func fun(value:T):any
+---@return fun(value:T):string
+local function wrapFunction(func) return function(x) return tostring(func(x)) end end
+
+---@param option _InnerPrettyOption
+---@return nil
+local function checkOption(option)
+    if not (type(option.lineBreakLimit) == 'number'
+            and option.lineBreakLimit >= 4
+            and option.lineBreakLimit <= maxSafeInteger / 2
+            and option.lineBreakLimit / 2 >= #option.indentString) then
+        error("\"lineBreakLimit\" is illegal")
+    end
+    if not (type(option.maximumNilNumberAllowed) == 'number'
+            and option.maximumNilNumberAllowed >= 0
+            and option.maximumNilNumberAllowed <= maxSafeInteger) then
+        error("\"maximumNilNumberAllowed\" is illegal")
+    end
+end
+
+---@param indentWidth any
+---@return string
+local function makeIndentString(indentWidth)
+    local num = tonumber(indentWidth)
+    if num == nil then error("not a number") end
+    return string.rep(" ", indentWidth)
+end
+
+---@param numberFormatter fun(value:number):string
+---@param integerFormatter fun(value:integer):string
+---@return fun(value:number):string
+local function makeNumberFormatter(numberFormatter, integerFormatter)
+    if numberFormatter == nil and integerFormatter == nil then
+        return num2string
+    end
+    numberFormatter = (numberFormatter ~= nil and wrapFunction(numberFormatter)) or num2string
+    integerFormatter = (integerFormatter ~= nil and wrapFunction(integerFormatter)) or num2string
+    return function(value)
+        return (checkUsingIntegerFormatter(value) and integerFormatter(value)) or numberFormatter(value)
+    end
+end
+
+---@param option PrettyOption|nil
+---@return _InnerPrettyOption
+local function mergeOption(option)
+    if option == nil then return globalPrettyOption end
+    if type(option) ~= 'table' then error("`option` is not a table") end
+    local retOption = copyTable(globalPrettyOption)
+    retOption["number"] = makeNumberFormatter(option.numberFormatter, option.integerFormatter)
+    if option.stringFormatter ~= nil then
+        retOption["string"] = wrapFunction(option.stringFormatter)
+    end
+    if option.booleanFormatter ~= nil then
+        retOption["boolean"] = wrapFunction(option.booleanFormatter)
+    end
+    if option.functionFormatter ~= nil then
+        retOption["function"] = wrapFunction(option.functionFormatter)
+    end
+    if option.userdataFormatter ~= nil then
+        retOption["userdata"] = wrapFunction(option.userdataFormatter)
+    end
+    if option.threadFormatter ~= nil then
+        retOption["thread"] = wrapFunction(option.threadFormatter)
+    end
+    if option.indentWidth ~= nil then
+        retOption.indentString = makeIndentString(option.indentWidth)
+    end
+    if option.lineBreakLimit ~= nil then
+        retOption.lineBreakLimit = tonumber(option.lineBreakLimit) or error("not an integer")
+    end
+    if option.maximumNilNumberAllowed ~= nil then
+        retOption.maximumNilNumberAllowed = tonumber(option.maximumNilNumberAllowed) or error("not an integer")
+    end
+    checkOption(retOption)
+    return retOption
+end
+
+---@param option PrettyOption
+---@return nil
+local function updateOption(option) globalPrettyOption = mergeOption(option) end
+
+---@return nil
+local function resetOption() globalPrettyOption = copyTable(backupGlobalPrettyOption) end
 
 ---Convert value to a pretty string.
 ---@param value any
+---@param option PrettyOption|nil
 ---@return string
-local function write(value)
-    local ret = writeInternal(value)
+local function write(value, option)
+    local mergedOption = mergeOption(option)
+    local ret = writeInternal(value, mergedOption)
     if type(ret) == "string" then return ret end
-    local s = maybeShortTable(ret, 0, indentWidth)
+    local s = maybeShortTable(ret, 0, #mergedOption.indentString, mergedOption.lineBreakLimit)
     return s or string.format("{\n%s\n}", table.concat(ret, "\n"))
 end
 
@@ -337,30 +467,31 @@ end
 ---  it cannot absolutely control the maximum length of each line.
 ---@param limit integer
 ---@return nil
-local function setLineBreakLimit(limit)
-    if limit * 2 >= maxSafeInteger then error("limit is too large") end
-    if limit <= 3 then error("limit is too small") end
-    if limit / 2 < indentWidth then error("limit is too small") end
-    lineBreakLimit = limit
-end
+local function setLineBreakLimit(limit) updateOption({ lineBreakLimit = limit }) end
 
 ---Set the indentation width.
 ---@param width integer
 ---@return nil
-local function setIndentWidth(width)
-    if width <= 0 then error("width is too small") end
-    if width * 2 >= lineBreakLimit then error("width is too large") end
-    indentWidth = width
-    indentString = string.rep(" ", width)
-end
+local function setIndentWidth(width) updateOption({ indentWidth = width }) end
 
 ---Set the maximum number of consecutive nil values allowed in an array;
 ---values exceeding this limit will be considered as part of the table.
 ---@param num integer
-local function setMaximumNilNumberAllowed(num)
-    if num < 0 then error("num is too small") end
-    if num >= maxSafeInteger then error("num is too large") end
-    maximumNilNumberAllowed = num
+local function setMaximumNilNumberAllowed(num) updateOption({ maximumNilNumberAllowed = num }) end
+
+---@param option PrettyOption|nil
+---@return fun(...):nil
+local function makePrinter(option)
+    local mergedOption = mergeOption(option)
+    return function(...)
+        local data = { ... }
+        printOne(data[1], mergedOption)
+        for i = 2, #data do
+            io.write("\t")
+            printOne(data[i], mergedOption)
+        end
+        io.write("\n")
+    end
 end
 
 ---Pretty print output.
@@ -368,9 +499,10 @@ end
 ---@return nil
 local function prettyPrint(...)
     local data = { ... }
-    for i = 1, #data do
-        if i > 1 then io.write("\t") end
-        printOne(data[i])
+    printOne(data[1], globalPrettyOption)
+    for i = 2, #data do
+        io.write("\t")
+        printOne(data[i], globalPrettyOption)
     end
     io.write("\n")
 end
@@ -380,7 +512,10 @@ local M = {
     setColumnLimit = setLineBreakLimit,
     setTabWidth = setIndentWidth,
     setMaximumNilNumberAllowed = setMaximumNilNumberAllowed,
-    print = prettyPrint
+    updateOption = updateOption,
+    resetOption = resetOption,
+    print = prettyPrint,
+    makePrinter = makePrinter,
 }
 return setmetatable(M, {
     __call = function(_, ...) return prettyPrint(...) end
